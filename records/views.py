9819c86e9a3491c63e73bafa28d31a1a6c5db09b
Rich Hart <richhohart@gmail.com>
2018-11-29 18:24:32 -0800

    add event for tsv parsing

--- records/views.py
+++ records/views.py
@@ -1,9 +1,19 @@
+import csv
+import codecs
 from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Document
 from .serializers import DocumentSerializer
+
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
+    def perform_create(self, serializer):
+#        import ipdb; ipdb.set_trace()
+        instance = serializer.save()
+        buffer_fp = codecs.iterdecode(instance.upload, 'utf-8') 
+        reader = csv.reader(buffer_fp, delimiter='\t')
+        for row in reader:
+            print(row)
