edacdb5cdac17b2387c240afd7d00ea4ed37ab71
Rich Hart <richhohart@gmail.com>
2018-11-29 16:20:27 -0800

    add document api

--- records/views.py
+++ records/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Document
+from .serializers import DocumentSerializer
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
