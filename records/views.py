ac69ffd1c589f44ae1e173d6c9892df6160e8a4c
Rich Hart <richhohart@gmail.com>
2018-11-29 19:47:47 -0800

    add parsing logic

--- records/views.py
+++ records/views.py
@@ -15,8 +15,19 @@ class DocumentViewSet(viewsets.ModelViewSet):
         instance = serializer.save()
         buffer_fp = codecs.iterdecode(instance.upload, 'utf-8') 
         reader = csv.reader(buffer_fp, delimiter='\t')
+        document = instance
         for row in reader:
-            print(row)
+            customer, _ = Customer.objects.update_or_create(id=row[0],first_name=row[1],last_name=row[2])
+            address, _ = Address.objects.update_or_create(street=row[3],state=row[4],zip_code=row[5])
+            product, _ = Product.objects.update_or_create(id=row[7], name=row[8], amount=row[9])
+            record = Record.objects.create(
+                document=document,
+                customer=customer,
+                address=address,
+                product=product,
+                status=row[6].upper()[0],
+                timestamp=row[10]
+            )
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
