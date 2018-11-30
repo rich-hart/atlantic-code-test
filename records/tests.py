6303226cc75a7ed4e190f90808edfd3088ef084a
Rich Hart <richhohart@gmail.com>
2018-11-29 18:34:57 -0800

    add tests

--- records/tests.py
+++ records/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import os
+from django.conf import settings
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+class TestDocument(TestCase):
+    def test_upload(self):
+        #import ipdb; ipdb.set_trace()
+        client = APIClient()
+        with open(os.path.join(settings.BASE_DIR, 'records','test_data.tsv')) as fp:
+            response = client.post('/documents/', {'upload': fp})
+        self.assertEqual(response.status_code, 201)
+
+
