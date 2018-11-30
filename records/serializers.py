f2df7b6bb41a8db32d1b41eebf03f731fe535ae9
Rich Hart <richhohart@gmail.com>
2018-11-29 19:47:25 -0800

    make read only

--- records/serializers.py
+++ records/serializers.py
@@ -11,18 +11,22 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('id','first_name','last_name')
+        read_only_fields = fields
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ('street','state','zip_code')
+        read_only_fields = fields
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'amount')
+        read_only_fields = fields
 
 class RecordSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Record
         fields = ('document','customer','address','product','status','timestamp')
+        read_only_fields = fields
